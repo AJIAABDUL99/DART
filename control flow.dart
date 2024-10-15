2a218681b4b21c50bf99f574a3ee8ad9e9e1d42e
Ajia Abdulhamid Mohammed <AJIAmoha254@gmail.com>
2024-10-15 11:06:49 +0300

    Commit Changes

---
+++ control flow.dart
@@ -0,0 +1,18 @@
+void main(){
+
+int result = 80;
+
+if (result >= 80){
+  print("You got an A, Excellent!");
+}else if (result >= 70 && result <= 79){
+  print("You got a B");
+}else if(result>= 60 && result<= 69){
+  print("You got a C");
+}else if(result>=50 && result<=59){
+  print("You got a D");
+}
+
+
+
+
+}
---
+++ switch statment.dart
@@ -0,0 +1,37 @@
+// void main(){
+//   String day ="Monday";
+//   switch(day){
+//     case "Monday":
+//     print("Start of the week.");
+//     break;
+//     case "Tuesday":
+//     print("Days until Wednesday.");
+//     break;
+//     case "Wednesday":
+//     print("Start of the work week.");
+//     break;
+//     case "Thursday":
+//     print("Days until Friday.");
+//     break;
+//     case "Friday":  
+//     print("Start of the weekend.");
+//     break;
+//   }
+// }
+void main(){
+  String Fruit ="Fruit";
+  switch(Fruit){
+    case "Apple":
+    print("Fruit of apple tree.");
+    break;
+    case "Banana":
+    print("Fruit of Muscovy bananas.");
+    break;
+    case "Orange":
+    print("Fruit of Citrus tree.");
+    break;
+    default:
+    print("Unknown fruit.");
+  
+}
+}
